ce50c4e9ed4c75d2dc18ce924c7a35ecf2e4e197
ashaforddesigns <ashaforddesigns@gmail.com>
2024-11-26 21:23:19 -0500

    setting up

---
+++ main.py
@@ -0,0 +1,6 @@
+""" Design classes for Book, Member, and Library.
+Add attributes and methods based on what you think these entities would need. For example:
+Books might have a title, author, and availability status.
+Members might have a name and a borrowing limit.
+Library might need to manage a collection of books and track transactions. """
+
