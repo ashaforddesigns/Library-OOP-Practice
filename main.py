8353165fcb88e84ed4c0adb0a996718a2d5b27c7
ashaforddesigns <ashaforddesigns@gmail.com>
2024-12-06 19:15:34 -0600

    testing

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ Books might have a title, author, and availability status.
 Members might have a name and a borrowing limit.
 Library might need to manage a collection of books and track transactions. """
 
+class Book:
+    def __init__(self, title, author=str, available=bool):
+        self.title = title
+        self.author = author
+        self.available = available
+
+class Member:
+    def __init__(self, name, checkedOut):
+        pass
+
+class Library:
+    def __init__(self):
+        pass
